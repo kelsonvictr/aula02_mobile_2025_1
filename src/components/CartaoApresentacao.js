6cc7b34a1c42a74237b27b1bb3f58a94f94c228b
kelsonalmeida <kelson.victr@gmail.com>
2025-02-27 19:59:38 -0300

    aula2 - andamento

---
+++ src/components/CartaoApresentacao.js
@@ -0,0 +1,61 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CartaoApresentacao = () => {
+  return (
+    <View>
+      <Text>CartaoApresentacao</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    // Container principal do cartão
+    container: {
+        width: 300,
+        padding: 20,
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4, 
+        elevation: 5,
+    },
+
+    imagemPerfil: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        margimBottom: 10,
+    },
+
+    nome: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#333',
+    },
+
+    descricao: {
+        fontSize: 14,
+        color: '#666',
+        textAlign: 'center',
+        marginVertical: 10,
+    },
+
+    botao: {
+        flex: 1,
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginHorizontal: 5,
+    },
+
+    botoesContainer: {
+        flexDirection: 'row',
+        marginTop: 10,
+    }
+})
+
+export default CartaoApresentacao
